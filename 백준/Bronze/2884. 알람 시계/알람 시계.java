cc97fdcce6b3a8357a9cbd9a5b5a9414f5691fcd
sujin oh <90545561+ohdeng02@users.noreply.github.com>
2024-03-08 12:52:07 +0900

    [Bronze III] Title: 알람 시계, Time: 236 ms, Memory: 18468 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt(); int m = sc.nextInt();
+        if(m<45){
+            m = 60+(m-45);
+            h--;
+            if(h<0) h=23;
+        }else m-=45;
+        System.out.print(h+" "+m);
+    }
+}

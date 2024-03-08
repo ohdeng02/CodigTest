9115c963494cc900b3cbae4652a123886ab488b0
sujin oh <90545561+ohdeng02@users.noreply.github.com>
2024-03-08 12:58:01 +0900

    [Bronze V] Title: 꼬마 정민, Time: 132 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder builder = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+        builder.append(Long.toString(a+b+c)+"\n");
+        bw.write(builder.toString()); bw.flush();
+    }
+}

732f37caef9c549823dac6d3717246a9d34371e4
sujin oh <90545561+ohdeng02@users.noreply.github.com>
2024-03-17 23:30:16 +0900

    [Silver V] Title: 돌 게임, Time: 128 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		bw.write((N % 2 == 1) ? "SK" : "CY");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}

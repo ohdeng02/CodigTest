331aa4efa54975a43fdb7b6567a2c3eee7c16726
sujin oh <90545561+ohdeng02@users.noreply.github.com>
2024-03-21 15:27:15 +0900

    [level 0] Title: 코드 처리하기, Time: 0.03 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -6,8 +6,7 @@ class Solution {
             if(code.charAt(i) == '1')
                 mode = (mode == 0 ? 1 : 0);
             else
-                if(mode == 0 && i%2==0) ret += code.charAt(i);
-                else if(mode == 1 && i%2!=0) ret += code.charAt(i);
+                if(i%2 == mode) ret += code.charAt(i);
         }
         return ret.isBlank() ? "EMPTY" : ret;
         //`isEmpty()`는 문자열의 길이가 0인지 확인합니다. - `isBlank()`는 문자열이 비어 있거나 공백 문자만 포함되어 있는지 확인

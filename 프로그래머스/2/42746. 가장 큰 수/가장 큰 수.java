fffd8fc4ab8818dc9e9d0e502406321a3ef627e0
Sujin <90545561+ohdeng02@users.noreply.github.com>
2024-04-08 11:45:35 +0900

    [level 2] Title: 가장 큰 수, Time: 1.54 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        
+        String answer = "";
+        String[] arr = new String[numbers.length];
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < numbers.length; i++) {
+            arr[i] = String.valueOf(numbers[i]);
+        }
+        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        
+        for (String s : arr) {
+            sb.append(s);
+        }
+        answer = sb.toString();
+        
+        if (answer.charAt(0) == '0') return "0";
+        return answer;
+    }
+}

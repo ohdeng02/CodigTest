31ae9fb30378288b4b1fe79091fc4d4fd8823e86
Sujin <90545561+ohdeng02@users.noreply.github.com>
2024-04-22 23:39:20 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.57 ms, Memory: 52.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int sum = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for(int i=0; i<A.length; i++){
+            sum+=A[i]*B[B.length-i-1];
+        }
+        
+        return sum;
+    }
+}

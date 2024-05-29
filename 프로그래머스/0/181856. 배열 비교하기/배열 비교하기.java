df641569758bb629cce4cfc8dcd8e6f9080aafc2
Sujin <90545561+ohdeng02@users.noreply.github.com>
2024-05-29 23:23:28 +0900

    [level 0] Title: 배열 비교하기, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -1,12 +1,17 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] arr1, int[] arr2) {
         int answer = 0;
         if (arr1.length == arr2.length) {
-            int sum1 = 0, sum2 = 0;
-            for (int i = 0; i < arr1.length; i++) {
-                sum1 += arr1[i];
-                sum2 += arr2[i];
-            }
+            //int sum1 = 0, sum2 = 0;
+            // for (int i = 0; i < arr1.length; i++) {
+            //     sum1 += arr1[i];
+            //     sum2 += arr2[i];
+            // }
+            int sum1 = Arrays.stream(arr1).sum();
+            int sum2 = Arrays.stream(arr2).sum();
+            
             if (sum1 != sum2) {
                 answer = sum1 > sum2 ? 1 : -1;
             }

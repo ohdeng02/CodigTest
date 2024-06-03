060570b43b97ee45359b5183d130695b933991e1
Sujin <90545561+ohdeng02@users.noreply.github.com>
2024-06-03 15:35:00 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.38 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -2,15 +2,7 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String my_string) {
-        String[] answer = {};
-        answer = my_string.split(" ");
-
-        List<String> list = new ArrayList<>();
-        for (String s : answer) {
-            if(!s.equals("")) {
-                list.add(s);
-            }
-        }
-        return list.toArray(String[]::new);
+        //String[] answer = {};
+        return my_string.strip().split("\\s+");
     }
 }

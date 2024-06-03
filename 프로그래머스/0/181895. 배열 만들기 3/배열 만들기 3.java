4b7263535336fc6bc0b735e8b779ffa9839323a9
Sujin <90545561+ohdeng02@users.noreply.github.com>
2024-06-03 21:18:18 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.77 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -1,13 +1,19 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] arr, int[][] intervals) {
-        int size = intervals[0][1]-intervals[0][0] + intervals[1][1] - intervals[1][0] + 2;
-        int[] answer = new int[size];
-        
+        List<Integer> list = new ArrayList<>();
         int idx = 0;
         for(int i = 0; i < intervals.length; i++) {
             for(int j = intervals[i][0]; j <= intervals[i][1]; j++) {
-                answer[idx++] = arr[j];
+                list.add(arr[j]);
             }
+        }
+        
+        int size = list.size();
+        int[] answer = new int[size];
+        for(int i = 0; i < size; i++) {
+            answer[i] = list.get(i);
         }
         return answer;
     }

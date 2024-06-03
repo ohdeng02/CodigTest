c7c0ae65b4bd1e77d47aa59a0a28548bd5d96c32
Sujin <90545561+ohdeng02@users.noreply.github.com>
2024-06-03 19:53:50 +0900

    [level 0] Title: 5명씩, Time: 0.01 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -4,12 +4,10 @@ class Solution {
     public String[] solution(String[] names) {
         
         int len = names.length;
-        //String[] answer = new String[len/5 + 1];
-        List<String> list = new ArrayList<>();
+        String[] answer = new String[(len-1)/5 + 1];
         for(int i = 0; i < len; i += 5) {
-            //answer[i/5] = names[i];
-            list.add(names[i]);
+            answer[i/5] = names[i];
         }
-        return list.toArray(String[]::new);
+        return answer;
     }
 }

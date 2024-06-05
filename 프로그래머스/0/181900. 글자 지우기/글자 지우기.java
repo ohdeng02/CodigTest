8df6f26e25d54b1a15cd32abde71078432e28ee1
Sujin <90545561+ohdeng02@users.noreply.github.com>
2024-06-05 23:32:33 +0900

    [level 0] Title: 글자 지우기, Time: 1.71 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -3,21 +3,16 @@ import java.util.*;
 class Solution {
     public String solution(String my_string, int[] indices) {
         
-        char[] charArr = my_string.toCharArray();
-        StringBuilder sb = new StringBuilder();
-        
-        Arrays.sort(indices);
-        int idx = 0;
-        int len = indices.length;
-        
-        for(int i = 0; i < my_string.length(); i++) {
-            if(i != indices[idx]) {
-                sb.append(charArr[i]);
-            } else {
-                idx = idx < len-1 ? idx + 1 : idx;
-            }
+        String answer = "";
+        String[] tmp = my_string.split("");
+
+        for (int i = 0; i < indices.length; i++) {
+            tmp[indices[i]] = "";
         }
-        
-        return sb.toString();
+
+        for (String x : tmp) {
+            answer += x;
+        }
+        return answer;
     }
 }

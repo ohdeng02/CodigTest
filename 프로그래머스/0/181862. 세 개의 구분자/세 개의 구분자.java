fae63664b7e450749e6e6520ab8938b138247931
Sujin <90545561+ohdeng02@users.noreply.github.com>
2024-06-07 20:25:48 +0900

    [level 0] Title: 세 개의 구분자, Time: 28.40 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,21 +1,36 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public String[] solution(String myStr) {
+//         String[] answer = {};
+//         myStr = myStr.replace("a", " "); 
+//         myStr = myStr.replace("b", " "); 
+//         myStr = myStr.replace("c", " "); 
+//         answer = myStr.split(" ");
+        
+//         List<String> list = new ArrayList<>();
+//         for(String s : answer) {
+//             if (!s.equals(" ") && !s.equals("")) {
+//                 list.add(s);   
+//             }
+//         }
+//         if (list.isEmpty()) list.add("EMPTY");
+        
+//         return list.toArray(String[]::new);
         String[] answer = {};
-        myStr = myStr.replace("a", " "); 
-        myStr = myStr.replace("b", " "); 
-        myStr = myStr.replace("c", " "); 
-        answer = myStr.split(" ");
-        
-        List<String> list = new ArrayList<>();
-        for(String s : answer) {
-            if (!s.equals(" ") && !s.equals("")) {
-                list.add(s);   
+        StringTokenizer st = new StringTokenizer(myStr,"abc");
+        if(st.countTokens() != 0){
+            answer = new String[st.countTokens()];
+            for(int i=0; i<answer.length; i++){
+                answer[i] = st.nextToken();
             }
+        }else{
+            answer = new String[1];
+            answer[0] = "EMPTY";
         }
-        if (list.isEmpty()) list.add("EMPTY");
-        
-        return list.toArray(String[]::new);
+
+        return answer;
     }
+    
 }

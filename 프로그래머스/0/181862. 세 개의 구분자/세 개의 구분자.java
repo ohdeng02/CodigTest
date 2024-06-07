1909fcb0865413f6afb42ddabd7086262434242f
Sujin <90545561+ohdeng02@users.noreply.github.com>
2024-06-07 20:43:15 +0900

    [level 0] Title: 세 개의 구분자, Time: 23.46 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,36 +1,21 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     public String[] solution(String myStr) {
-//         String[] answer = {};
-//         myStr = myStr.replace("a", " "); 
-//         myStr = myStr.replace("b", " "); 
-//         myStr = myStr.replace("c", " "); 
-//         answer = myStr.split(" ");
-        
-//         List<String> list = new ArrayList<>();
-//         for(String s : answer) {
-//             if (!s.equals(" ") && !s.equals("")) {
-//                 list.add(s);   
-//             }
-//         }
-//         if (list.isEmpty()) list.add("EMPTY");
-        
-//         return list.toArray(String[]::new);
         String[] answer = {};
         StringTokenizer st = new StringTokenizer(myStr,"abc");
-        if(st.countTokens() != 0){
-            answer = new String[st.countTokens()];
-            for(int i=0; i<answer.length; i++){
+        int n = st.countTokens();
+        
+        if (n != 0) {
+            answer = new String[n];
+            for(int i = 0; i < n; i++) {
                 answer[i] = st.nextToken();
             }
-        }else{
+        } else {
             answer = new String[1];
             answer[0] = "EMPTY";
         }
 
         return answer;
     }
-    
 }

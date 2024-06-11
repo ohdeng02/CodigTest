29de434bad7b847acfa6564b8be109d85a8bae36
Sujin <90545561+ohdeng02@users.noreply.github.com>
2024-06-11 17:24:47 +0900

    [level 0] Title: 2의 영역, Time: 2.05 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -1,34 +1,59 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] arr) {
-        int[] answer;
-        int firstIdx = 0, lastIdx = 0;
-        int count = 0;
+        // int[] answer;
+        //List<Integer> answer = new ArrayList<>();
+//         int firstIdx = 0, lastIdx = 0;
+//         int count = 0;
     
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 2) {
-                firstIdx = i;
-                count++;
-                break;
+//         for (int i = 0; i < arr.length; i++) {
+//             if (arr[i] == 2) {
+//                 firstIdx = i;
+//                 count++;
+//                 break;
+//             }
+//         }
+
+//         for (int i = arr.length-1; i >= firstIdx; i--) {
+//             if (arr[i] == 2) {
+//                 lastIdx = i;
+//                 break;
+//             }
+//         }        
+        
+//         if (count == 0) {
+//             answer = new int[] {-1};
+//         } else {
+//             answer = new int[lastIdx - firstIdx + 1];
+//             int idx = 0;
+//             for (int i = firstIdx; i <= lastIdx; i++) {
+//                 answer[idx++] = arr[i];
+//             }
+//         }
+        
+//         return answer;
+
+        int[] answer = {};
+        int start = -1;
+        int end = -1;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==2){
+                if(start==-1){
+                    start = i;
+                }
+                end = i;
             }
         }
 
-        for (int i = arr.length-1; i >= firstIdx; i--) {
-            if (arr[i] == 2) {
-                lastIdx = i;
-                break;
-            }
-        }        
-        
-        if (count == 0) {
-            answer = new int[] {-1};
-        } else {
-            answer = new int[lastIdx - firstIdx + 1];
-            int idx = 0;
-            for (int i = firstIdx; i <= lastIdx; i++) {
-                answer[idx++] = arr[i];
-            }
+        if(start==-1){
+            answer = new int[]{-1};
+        }else{
+
+            answer = Arrays.copyOfRange(arr,start,end+1);
         }
-        
+
         return answer;
     }
 }

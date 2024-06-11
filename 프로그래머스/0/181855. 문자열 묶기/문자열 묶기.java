a5f0924b8ed7eb303d1232eef2b3925084bbdb8b
Sujin <90545561+ohdeng02@users.noreply.github.com>
2024-06-11 16:20:30 +0900

    [level 0] Title: 문자열 묶기, Time: 6.50 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -2,14 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] strArr) {        
-        int[] arr = new int[31];
-        for (String s : strArr) {
-            arr[s.length()]++;
+//         int[] arr = new int[31];
+//         for (String s : strArr) {
+//             arr[s.length()]++;
+//         }
+        
+//         Arrays.sort(arr);
+//         int answer = arr[arr.length-1];
+        
+//         return answer;
+        int answer = 0;
+
+        int[] lengArr = new int[31];
+
+        for(int i=0; i<strArr.length; i++) {
+            lengArr[strArr[i].length()]++;
         }
-        
-        Arrays.sort(arr);
-        int answer = arr[arr.length-1];
-        
+
+        for(int i=0; i<=30; i++) {
+            answer = Math.max(answer, lengArr[i]);
+        }
+
         return answer;
     }
 }

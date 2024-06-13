3a863cae8fce0cdcc4271e479202bfbea7d46db7
Sujin <90545561+ohdeng02@users.noreply.github.com>
2024-06-13 18:26:14 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -9,18 +9,19 @@ class Solution {
         
         for (int i = 0; i < len; i++) {
             if (str_list[i].equals("l")) {
-                answer = new String[i];
-                for (int j = 0; j < i; j++) {
-                    answer[idx++] = str_list[j];
-                }
-                break;
-                
+                // answer = new String[i];
+                // for (int j = 0; j < i; j++) {
+                //     answer[idx++] = str_list[j];
+                // }
+                // break;
+                return Arrays.copyOfRange(str_list, 0, i);
             } else if (str_list[i].equals("r")) {
-                answer = new String[len-i-1];
-                for (int j = i + 1; j < len; j++) {
-                    answer[idx++] = str_list[j];
-                }
-                break;
+                // answer = new String[len-i-1];
+                // for (int j = i + 1; j < len; j++) {
+                //     answer[idx++] = str_list[j];
+                // }
+                // break;
+                return Arrays.copyOfRange(str_list, i + 1, len);
             }
         }
         

cb9ba461da5e516a62fc45c9c8f6ddff6ba94c7d
Sujin <90545561+ohdeng02@users.noreply.github.com>
2024-06-13 17:54:43 +0900

    [level 0] Title: 배열 만들기 6, Time: 12.27 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -3,32 +3,24 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
         
-        int[] answer;
-        List<Integer> list = new ArrayList<>();
-        int len = arr.length;
-        int i = 0;
-        
-        while (i < len) {
-            if (list.isEmpty()) {
-                list.add(arr[i]);
-            } else if (!list.isEmpty() && list.get(list.size()-1) == arr[i]) {
-                list.remove(list.size()-1);
+        Stack<Integer> stk = new Stack<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (stk.isEmpty()) {
+                stk.push(arr[i]);
+            } else if (stk.peek() == arr[i]) {
+                stk.pop();
             } else {
-                list.add(arr[i]);
+                stk.push(arr[i]);
             }
-            i++;
         }
+        if (stk.isEmpty()) stk.push(-1);
         
-        int size = list.size();
-        if (size == 0) {
-            answer = new int[] {-1};
-        } else {
-            answer = new int[size];
-            for (int j = 0; j < size; j++) {
-                answer[j] = list.get(j);
-            }   
+        int size = stk.size();
+        int[] answer = new int[size];
+        for (int j = 0; j < size; j++) {
+            answer[j] = stk.get(j);
         }
-        
         return answer;
     }
 }

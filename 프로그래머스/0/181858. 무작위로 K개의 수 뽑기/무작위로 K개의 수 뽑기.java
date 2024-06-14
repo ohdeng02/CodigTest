3f7992590ba6ba2f8e2030ed08f5163b5766cdbb
Sujin <90545561+ohdeng02@users.noreply.github.com>
2024-06-14 17:58:58 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 5.51 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        
+        int[] answer = new int[k];
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i : arr) {
+            if (!list.contains(i)) {
+                list.add(i);
+            }
+            if (list.size() == k) break;
+        }
+        
+        for (int i = 0; i < k; i++) {
+            if (list.size() - 1 < i) {
+                answer[i] = -1;
+            } else {
+                answer[i] = list.get(i);   
+            }            
+        }
+        return answer;
+    }
+}

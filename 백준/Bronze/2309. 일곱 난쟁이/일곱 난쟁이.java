3baeed708f001601ccab0ee107eda22b0070f2e8
sujin oh <90545561+ohdeng02@users.noreply.github.com>
2024-10-15 12:52:46 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 104 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,30 +1,33 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
 class Main {
+    private static int[] heights = new int[9];
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int[] arr = new int[9];
         int sum = 0;
-
-        for(int i=0; i<9; i++){
-            arr[i] = Integer.parseInt(br.readLine());
-            sum += arr[i];
-        }
         
-        int goal = sum - 100;
-        for(int i = 0; i < 8; i++){
-            for(int j = i+1; j < 9; j++){
-                if(arr[i] + arr[j] == goal) {
-                    arr[i] = 0; 
-                    arr[j] = 0;
-                    Arrays.sort(arr);
-                    for(int k = 2; k < 9; k++){
-                        //sb.append(arr[k] + "\n");
-                        System.out.println(arr[k]);
-                    }
-                    
+        for (int i = 0; i < 9; i++) {
+            heights[i] = Integer.parseInt(br.readLine());
+            sum += heights[i];
+        }
+        findDwarf(sum);
+
+        Arrays.sort(heights);
+        for (int i = 2; i < 9; i++) {
+            System.out.println(heights[i]);
+        }
+    }
+
+    public static void findDwarf(int sum) {
+        //전체합 - 2명 = 100
+        for (int i = 0; i < 8; i++) {
+            for (int j = i+1; j < 9; j++) {
+                if (heights[i] + heights[j] == sum - 100) {
+                    heights[i] = 0;
+                    heights[j] = 0;
                     return;
                 }
             }

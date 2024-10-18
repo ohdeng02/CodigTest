2c1f907ea770a78b2ee844f2efed43c5f1eaa101
sujin oh <90545561+ohdeng02@users.noreply.github.com>
2024-10-19 01:32:53 +0900

    [Silver II] Title: 참외밭, Time: 104 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.java
+++ 백준/Silver/2477. 참외밭/참외밭.java
@@ -1,46 +1,46 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
 class Main {
-    static int[] length;
+    private static int[] lengths = new int[6];
     
-    static int cuttedLength(int idx) {
-        int len = 0;
-        if(idx == 0){
-            len = Math.abs(length[5] - length[1]);
-        } else if (idx == 5){
-            len = Math.abs(length[0] - length[4]);
+    public static int findCutted(int index) {
+        if (index == 0) {
+            return Math.abs(lengths[index + 1] - lengths[5]);
+        } else if (index == 5) {
+            return Math.abs(lengths[index - 1] - lengths[0]);
         } else {
-            len = Math.abs(length[idx-1] - length[idx+1]);
+            return Math.abs(lengths[index - 1] - lengths[index + 1]);
         }
-        return len;
     }
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        length = new int[6];
-        int K = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
 
-        int maxHeight = 0;
-        int maxWidth = 0;
-        int heightIdx = 0, widthIdx = 0;
-        
-        for(int i = 0; i < 6; i++){
-            st = new StringTokenizer(br.readLine());
-            int direction = Integer.parseInt(st.nextToken());
-            length[i] = Integer.parseInt(st.nextToken());
+        int maxWidth = 0, maxHeight = 0;
+        int widthIdx = 0, heightIdx = 0;
+        for (int i = 0; i < 6; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-            if((direction == 1 || direction == 2) && maxWidth < length[i]) {
-                maxWidth = length[i];
-                widthIdx = i;
-            } else if((direction == 3 || direction == 4) && maxHeight < length[i]) {
-                maxHeight = length[i];
-                heightIdx = i;
+            int dir = Integer.parseInt(st.nextToken());
+            lengths[i] = Integer.parseInt(st.nextToken());
+            if (dir == 1 || dir == 2) {
+                if (lengths[i] > maxWidth) {
+                    maxWidth = lengths[i];
+                    widthIdx = i;
+                }
+            } else {
+                if (lengths[i] > maxHeight) {
+                    maxHeight = lengths[i];
+                    heightIdx = i;
+                }
             }
         }
-        int cuttedArea = cuttedLength(heightIdx) * cuttedLength(widthIdx);
-        int result = (maxHeight * maxWidth - cuttedArea) * K;
-        System.out.println(result);
+
+        int cutWidth = findCutted(heightIdx);
+        int cutHeight = findCutted(widthIdx);
+        System.out.println(k * (maxWidth * maxHeight - cutWidth * cutHeight));
     }
 }

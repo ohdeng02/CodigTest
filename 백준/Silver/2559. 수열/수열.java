2db93a918f32c81e8106cbdb2876b6e064c088ad
sujin oh <90545561+ohdeng02@users.noreply.github.com>
2024-10-24 17:44:54 +0900

    [Silver III] Title: 수열, Time: 996 ms, Memory: 23808 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i <= N - K; i++) {
+            int sum = 0;
+            for (int j = i; j < i + K; j++) {
+                sum += arr[j];
+            }
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        System.out.println(maxSum);
+    }
+}

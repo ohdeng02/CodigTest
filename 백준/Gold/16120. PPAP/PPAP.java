d017d9f49c0bbe2e9c1af3dea1432db92761e473
sujin oh <90545561+ohdeng02@users.noreply.github.com>
2024-11-08 10:40:43 +0900

    [Gold IV] Title: PPAP, Time: 768 ms, Memory: 219676 KB -BaekjoonHub

--- 백준/Gold/16120. PPAP/PPAP.java
+++ 백준/Gold/16120. PPAP/PPAP.java
@@ -1,35 +1,33 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
 class Main {
     static Stack<Character> stack = new Stack<>();
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
-
+        
         String result = "NP";
+		String word = "PPAP";
         for (int i = 0; i < str.length(); i++) {
             stack.push(str.charAt(i));
             int size = stack.size();
             if (size >= 4) {
                 StringBuilder sb = new StringBuilder();
-                for (int j = 4; j > 0; j--) {
+                for (int j = word.length(); j > 0; j--) {
                     sb.append(stack.get(size - j));
                 }
-                if (sb.toString().equals("PPAP")) {
+                if (sb.toString().equals(word)) {
                     popChar();
                 }
             }
         }
-
         if (stack.size() == 1 && stack.peek() == 'P') {
-            result = "PPAP";
+            result = word;
         }
         System.out.println(result);
     }
-
     public static void popChar() {
         for (int i = 0; i < 3; i++) {
             stack.pop();

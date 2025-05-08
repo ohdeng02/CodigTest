91d658ba002f18dc2872089ea7b0aa69a5210114
sujin oh <90545561+ohdeng02@users.noreply.github.com>
2025-05-08 16:37:20 +0900

    [Silver I] Title: 오르막 수, Time: 112 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.java
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -1,27 +1,26 @@
-import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int[][] dp = new int[N+1][10];
-		
-		for(int i = 0; i < 10; i++) {
-			dp[0][i] = 1;
-		}
-		
-	
-		for(int i = 1; i < N+1; i++) {
-			for(int j = 0; j < 10; j++) {
-				for(int k = j; k < 10; k++) {
-					dp[i][j] += dp[i-1][k];
-					dp[i][j] %= 10007;
-				}
-			}
-		}
-		
-		System.out.println(dp[N][0] % 10007); 
-	}
+        long[][] dp = new long[N + 1][10];
+        for (int i = 0; i < 10; i++) {
+            dp[0][i] = 1;
+        }
 
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = j; k < 10; k++) {
+                    dp[i][j] += dp[i-1][k];
+                    dp[i][j] %= 10007;
+                }
+            }
+        }
+
+        System.out.println(dp[N][0] % 10007);
+    }
 }

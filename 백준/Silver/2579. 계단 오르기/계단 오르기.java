eec30594a88f47c1f115fc9453d4f7c3f19e0d75
sujin oh <90545561+ohdeng02@users.noreply.github.com>
2025-05-09 15:43:54 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,36 +1,27 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
- 
-public class Main {
- 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		int N = Integer.parseInt(br.readLine());
- 
-		int[] DP = new int[N + 1];
-		int[] arr = new int[N + 1];
- 
- 
-		for (int i = 1; i <= N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
- 
-		// index = 0 은 시작점이다.
-		DP[1] = arr[1];
-		
-		// N 이 1이 입력될 수도 있기 때문에 예외처리를 해줄 필요가 있다.
-		if (N >= 2) {
-			DP[2] = arr[1] + arr[2];
-		}
- 
-		for (int i = 3; i <= N; i++) {
-			DP[i] = Math.max(DP[i - 2] , DP[i - 3] + arr[i - 1]) + arr[i];
-		}
- 
-		System.out.println(DP[N]);
- 
-	}
- 
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] scores = new int[N + 1];
+        int[] dp = new int[N + 1];
+        
+        for (int i = 1; i <= N; i++) {
+            scores[i] = Integer.parseInt(br.readLine());
+        }
+
+        dp[1] = scores[1];
+        if (N >= 2) {
+            dp[2] = dp[1] + scores[2];
+        }
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = Math.max(dp[i - 2], dp[i - 3] + scores[i - 1]) + scores[i];
+        }
+        
+        System.out.println(dp[N]);
+    }
 }
